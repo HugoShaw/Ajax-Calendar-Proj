e6e85298f94075f7f4393d9fbbc6f2fb685a3eb9
HugoShaw <44939548+HugoShaw@users.noreply.github.com>
2022-04-18 21:04:57 -0500

    Delete database.php

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli('localhost', 'wustl_inst', 'wustl_pass', 'calendar');
-
-if($mysqli->connect_errno) {
-	printf("Connection Failed: %s\n", $mysqli->connect_error);
-	exit;
-} 
-
-?>
